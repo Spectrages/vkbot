e202aca0277d67df993ecf9d3cc19b7aac7dd68a
Spectrages <spectrages@gmail.com>
2023-07-14 14:02:27 +0300

    Fix

--- src/app.service.ts
+++ src/app.service.ts
@@ -6,17 +6,20 @@ import * as https from 'https';
 @Injectable()
 export class AppService {
   private vk: VK;
+  private users: string[];
 
   constructor() {
     this.vk = new VK({
       token:
         'vk1.a.5cLvNW-YUVmtpsPcCotF2l7IdI7ofG8Ne_HSlRdEKmFTiIo4KmbTcs2LX7_ZrttS1mHbJRKirbmDctveB-CroX-WFUBjYUaRMy1gPxVEfuy6-2rEgdx6B--jS47NAnafDn7FojRs1bVuP9ySHEON0SbA_qMmPdqL7Fu401zqhIqXfN7YCJ5tTKyU40PfV7yoYbb3V9lqh7thQG1EBv4gCA',
     });
+
+    this.users = ['spectrages', 'snusmumrick'];
   }
 
   onModuleInit() {
     this.vk.updates.on('message_new', async (context) => {
-      if (context.text.toLowerCase() === 'test message') {
+      if (context.text.toLowerCase() === 'привет') {
         try {
           await context.send('Привет, я работаю');
         } catch (error) {
@@ -28,7 +31,7 @@ export class AppService {
     this.vk.updates
       .startPolling()
       .then(() => {
-        console.log('Bot started');
+        console.log('Bot restarted');
       })
       .catch(console.error);
   }
@@ -52,13 +55,16 @@ export class AppService {
 
   @Cron('0 */3 * * *')
   async sendWeatherForecast() {
-    const userId = await this.getUserId('spectrages');
     const city = 'Minsk';
     const forecast = await this.getWeatherForecast(city);
-    await this.vk.api.messages.send({
-      user_id: userId,
-      random_id: Math.floor(Math.random() * 1e9),
-      message: forecast,
+
+    return await this.users.forEach(async (screenName: string) => {
+      const userId = await this.getUserId(screenName);
+      await this.vk.api.messages.send({
+        user_id: userId,
+        random_id: Math.floor(Math.random() * 1e9),
+        message: forecast,
+      });
     });
   }
 
